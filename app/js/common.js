2e687d0ec97a1d94b3a01db03a3d722347a4dcb8
Alexey <leha-sartana2@yandex.ru>
2018-05-16 19:45:20 +0300

    masks + fonts

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,14 @@ $(function() {
         margin:10,
         nav:false,
         items: 1
-    })
+    });
+
+
+    $("#showCallback").on('click', function () {
+        var $modal = $("#callbackModal");
+        $modal.modal('show');
+    });
+
+    $('.phone').mask('+7 (000) 000-00-00');
 
 });
